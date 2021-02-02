731bd4d8f97495ceda62a1ee55523234905f084b
ferdinand <ferdinandmwal@gmail.com>
2021-02-02 13:18:36 +0300

    1

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,10 +1,12 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      title: 'Candidates',
+      debugShowCheckedModeBanner: false,
     );
   }
 }
