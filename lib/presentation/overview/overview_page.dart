738d4dfbd49ce84adb939fdc0ec3b0676e44fa0b
ferdinand <ferdinandmwal@gmail.com>
2021-02-02 13:37:45 +0300

    set up candidate page

--- lib/presentation/overview/overview_page.dart
+++ lib/presentation/overview/overview_page.dart
@@ -1,3 +1,4 @@
+import 'package:candidate_central/presentation/overview/widgets/candidate_cards_overview_page.dart';
 import 'package:flutter/material.dart';
 
 class CandidateOverviewPage extends StatelessWidget {
@@ -6,8 +7,16 @@ class CandidateOverviewPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Candidates'),
-        
+        leading: IconButton(
+          icon: const Icon(Icons.exit_to_app),
+          onPressed: () {
+            //TODO:(O1) When exit app is pressed
+          },
+        ),
+        //TODO:(02)Uncompleted switch for candidates who are still in
+        //*actions: [],
       ),
+      body: CandidateCardsOverviewPage(),
     );
   }
 }
