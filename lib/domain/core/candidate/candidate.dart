1a71e0d949526c0530abde810fb1fe355987ea1a
ferdinand <ferdinandmwal@gmail.com>
2021-02-03 12:36:24 +0300

    Created candidate dataclass

---
+++ lib/domain/core/candidate/candidate.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/painting.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'candidate.freezed.dart';
+
+@freezed
+abstract class Candidate with _$Candidate {
+  const factory Candidate({
+    @required AssetImage face,
+    @required String candidateName,
+    @required int age,
+  }) = _Candidate;
+}
