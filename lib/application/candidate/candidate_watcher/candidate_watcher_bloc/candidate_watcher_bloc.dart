34726ff985a1b08c8d855a1822602cc00255cb14
ferdinand <ferdinandmwal@gmail.com>
2021-02-06 12:15:27 +0300

    Added dependencies for bloc

--- lib/application/candidate/candidate_watcher/candidate_watcher_bloc/candidate_watcher_bloc.dart
+++ lib/application/candidate/candidate_watcher/candidate_watcher_bloc/candidate_watcher_bloc.dart
@@ -8,8 +8,7 @@ part 'candidate_watcher_event.dart';
 part 'candidate_watcher_state.dart';
 part 'candidate_watcher_bloc.freezed.dart';
 
-class CandidateWatcherBloc
-    extends Bloc<CandidateWatcherEvent, CandidateWatcherState> {
+class CandidateWatcherBloc extends Bloc<CandidateWatcherEvent, CandidateWatcherState> {
   CandidateWatcherBloc() : super(_Initial());
 
   @override
