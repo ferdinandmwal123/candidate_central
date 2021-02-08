07c41ce80a507095896587acc2c0afadc00ccd88
ferdinand <ferdinandmwal@gmail.com>
2021-02-08 14:46:46 +0300

    Finished refactoring test

--- lib/presentation/overview/overview_page.dart
+++ lib/presentation/overview/overview_page.dart
@@ -1,6 +1,8 @@
 import 'package:candidate_central/candidate_data.dart';
 import 'package:candidate_central/domain/core/candidate/candidate.dart';
 import 'package:candidate_central/presentation/overview/widgets/candidate_card.dart';
+import 'package:candidate_central/presentation/overview/widgets/candidate_cards_overview_page.dart';
+import 'package:candidate_central/presentation/overview/widgets/overview_top_design.dart';
 import 'package:candidate_central/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -22,46 +24,14 @@ class CandidateOverviewPage extends StatelessWidget {
       body: SingleChildScrollView(
           child: Stack(
         children: [
-          Container(
-            padding: EdgeInsets.all(40),
-            constraints: BoxConstraints.expand(height: 225),
-            decoration: BoxDecoration(
-                gradient: new LinearGradient(
-                    colors: [lightBlueIsh, lightGreen],
-                    begin: const FractionalOffset(1.0, 1.0),
-                    end: const FractionalOffset(0.2, 0.2),
-                    stops: [0.0, 1.0],
-                    tileMode: TileMode.clamp),
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(30),
-                    bottomRight: Radius.circular(30))),
-            child: Container(
-              padding: EdgeInsets.only(top: 50),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Find Your Candidate',
-                    style: titleStyleWhite,
-                  )
-                ],
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 120),
-            constraints: BoxConstraints.expand(height: 200),
-            child: ListView(
-                padding: EdgeInsets.only(left: 40),
-                scrollDirection: Axis.horizontal,
-                children: getRecentcandidates()),
-          ),
+          OverviewDesign(),
+          CandidateCardsOverviewPage(),
           Container(
             height: 500,
-            margin: EdgeInsets.only(top: 300),
-            padding: EdgeInsets.all(20),
+            margin: const EdgeInsets.only(top: 300),
+            padding: const EdgeInsets.all(20),
             child: Container(
-              margin: EdgeInsets.only(top: 40),
+              margin: const EdgeInsets.only(top: 40),
               child: Text(
                 "Explore New Opportunities",
                 style: titileStyleBlack,
@@ -71,41 +41,5 @@ class CandidateOverviewPage extends StatelessWidget {
         ],
       )),
     );
-  }
-
-  List<Candidate> findcandidates() {
-    List<Candidate> candidates = [];
-
-    candidates.add(new Candidate(
-        face: AssetImage("lib/assets/trump.jpg"),
-        candidateName: "Donald Trump Sr",
-        age: 75,
-        party: "Rep."));
-    candidates.add(new Candidate(
-        age: 24,
-        candidateName: "Hilary Clinton",
-        face: AssetImage("lib/assets/clinton.jpg"),
-        party: "Dem."));
-
-    candidates.add(new Candidate(
-        age: 98,
-        candidateName: "Joe Biden",
-        face: AssetImage("lib/assets/biden.jpg"),
-        party: "Dem."));
-
-    return candidates;
-  }
-
-  List<Widget> getRecentcandidates() {
-    List<Widget> recentcandidateCards = [];
-    List<Candidate> candidates = findcandidates();
-    for (Candidate candidate in candidates) {
-      recentcandidateCards.add(getcandidateCard(candidate));
-    }
-    return recentcandidateCards;
-  }
-
-  Widget getcandidateCard(Candidate candidate) {
-    return  CandidateCard(candidate: candidate);
   }
 }
