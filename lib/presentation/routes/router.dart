7e539994936c0d9864194eacf2e62696d792ed30
ferdinand <ferdinandmwal@gmail.com>
2021-02-12 13:27:33 +0300

    Passing args

--- lib/presentation/routes/router.dart
+++ lib/presentation/routes/router.dart
@@ -1,9 +1,11 @@
 import 'package:auto_route/auto_route_annotations.dart';
+import 'package:candidate_central/presentation/candidate_page.dart';
 import 'package:candidate_central/presentation/overview/overview_page.dart';
 
 @MaterialAutoRouter(
   routes: <AutoRoute>[
-    MaterialRoute(page:CandidateOverviewPage, initial: true)
+    MaterialRoute(page:CandidateOverviewPage, initial: true),
+    MaterialRoute(page: CandidatePage(),),
   ],
 )
 class $Router {}
