4bf06deca58fd908691d7fa324be762036fe24b4
ferdinand <ferdinandmwal@gmail.com>
2021-02-15 13:57:41 +0300

    Working candidate photo & popuntil added

--- lib/presentation/single_candidate_page/candidate_page.dart
+++ lib/presentation/single_candidate_page/candidate_page.dart
@@ -1,8 +1,9 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:candidate_central/domain/core/candidate/candidate.dart';
-import 'package:candidate_central/presentation/overview/widgets/overview_top_design.dart';
+import 'package:candidate_central/presentation/routes/router.gr.dart';
 import 'package:flutter/material.dart';
-
 import 'widgets/single_candidate_card.dart';
+
 
 class CandidatePage extends StatelessWidget {
   final Candidate candidate;
@@ -15,15 +16,14 @@ class CandidatePage extends StatelessWidget {
         title: Text("${candidate.candidateName}'s page"),
         leading: IconButton(
             icon: const Icon(Icons.arrow_back_ios), onPressed: () {
-
+              ExtendedNavigator.of(context).popUntil((route) => route.settings.name == Routes.candidateOverviewPage );
             }),
       ),
       body: SingleChildScrollView(
         child: Stack(
           children: [
-            const OverviewDesign(
-              text: "welcome dawg",
-            ),
+            CircleAvatar(backgroundImage: candidate.face),
+            Text(candidate.candidateName),
             SingleCandidateCard(),
           ],
         ),
--- lib/presentation/single_candidate_page/widgets/single_candidate_card.dart
+++ lib/presentation/single_candidate_page/widgets/single_candidate_card.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class SingleCandidateCard extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return const Card(
+
     );
   }
 }
