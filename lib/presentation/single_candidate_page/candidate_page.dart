397c88e0aedd5ab5d891b6caf90748276eac933d
ferdinand <ferdinandmwal@gmail.com>
2021-02-15 13:24:02 +0300

    Created single candidate card

--- lib/presentation/single_candidate_page/candidate_page.dart
+++ lib/presentation/single_candidate_page/candidate_page.dart
@@ -2,19 +2,29 @@ import 'package:candidate_central/domain/core/candidate/candidate.dart';
 import 'package:candidate_central/presentation/overview/widgets/overview_top_design.dart';
 import 'package:flutter/material.dart';
 
+import 'widgets/single_candidate_card.dart';
+
 class CandidatePage extends StatelessWidget {
   final Candidate candidate;
   const CandidatePage({Key key, this.candidate}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("${candidate.candidateName}'s page"),
+        leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios), onPressed: () {
+
+            }),
+      ),
       body: SingleChildScrollView(
         child: Stack(
           children: [
             const OverviewDesign(
               text: "welcome dawg",
             ),
+            SingleCandidateCard(),
           ],
         ),
       ),
---
+++ lib/presentation/single_candidate_page/widgets/single_candidate_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SingleCandidateCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
