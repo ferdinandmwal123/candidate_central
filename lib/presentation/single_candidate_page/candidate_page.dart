060139f163b33c0b004aef1a5689ff262d88eb68
ferdinand <ferdinandmwal@gmail.com>
2021-02-16 13:28:45 +0300

    Centered

--- lib/presentation/single_candidate_page/candidate_page.dart
+++ lib/presentation/single_candidate_page/candidate_page.dart
@@ -4,7 +4,6 @@ import 'package:candidate_central/presentation/routes/router.gr.dart';
 import 'package:flutter/material.dart';
 import 'widgets/single_candidate_card.dart';
 
-
 class CandidatePage extends StatelessWidget {
   final Candidate candidate;
   const CandidatePage({Key key, this.candidate}) : super(key: key);
@@ -15,12 +14,14 @@ class CandidatePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("${candidate.candidateName}'s page"),
         leading: IconButton(
-            icon: const Icon(Icons.arrow_back_ios), onPressed: () {
-              ExtendedNavigator.of(context).popUntil((route) => route.settings.name == Routes.candidateOverviewPage );
+            icon: const Icon(Icons.arrow_back_ios),
+            onPressed: () {
+              ExtendedNavigator.of(context).popUntil((route) =>
+                  route.settings.name == Routes.candidateOverviewPage);
             }),
       ),
-      body: SingleChildScrollView(
-        child: Stack(
+      body: Center(
+        child: Column(
           children: [
             CircleAvatar(backgroundImage: candidate.face),
             Text(candidate.candidateName),
