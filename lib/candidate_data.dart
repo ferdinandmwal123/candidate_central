1b776c628d2565d0a6c0bffab86a0594eeed67c8
ferdinand <ferdinandmwal@gmail.com>
2021-02-22 14:54:03 +0300

    working

---
+++ lib/candidate_data.dart
@@ -0,0 +1,18 @@
+const FOOD_DATA = [
+  {"name": "Burger", "brand": "Hawkers", "price": 2.99, "image": "trump.jpg"},
+  {
+    "name": "Cheese Dip",
+    "brand": "Hawkers",
+    "price": 4.99,
+    "image": "clinton.jpg"
+  },
+  {"name": "Cola", "brand": "Mcdonald", "price": 1.49, "image": "biden.jpg"},
+  {"name": "Burger", "brand": "Hawkers", "price": 2.99, "image": "trump.jpg"},
+  {
+    "name": "Cheese Dip",
+    "brand": "Hawkers",
+    "price": 4.99,
+    "image": "clinton.jpg"
+  },
+  {"name": "Cola", "brand": "Mcdonald", "price": 1.49, "image": "biden.jpg"},
+];
