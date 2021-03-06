f919316dd4731f29c0242b0110e30eeba7824d1c
ferdinand <ferdinandmwal@gmail.com>
2021-03-06 15:02:22 +0300

    !

--- lib/candidate_data.dart
+++ lib/candidate_data.dart
@@ -7,6 +7,9 @@ const candidateData = [
     "face": "trump.jpg",
     "migration-policy":
         "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+    "social-policy":
+        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+        "slogan":"Make America Great Again",
     "tax-policy":
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
   },
@@ -15,6 +18,11 @@ const candidateData = [
     "age": 88,
     "party": "Democrat",
     "face": "biden.jpg",
+    "migration-policy":
+        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+    "social-policy":
+        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+        "slogan":"Make America Great Again",
     "tax-policy":
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
   },
