761e4f5ed501852acf17e30ad064d049cd8f66cb
ferdinand <ferdinandmwal@gmail.com>
2021-03-06 14:44:46 +0300

    Migration policy

--- lib/candidate_data.dart
+++ lib/candidate_data.dart
@@ -5,6 +5,8 @@ const candidateData = [
     "age": 75,
     "party": "Republican",
     "face": "trump.jpg",
+    "migration-policy":
+        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
     "tax-policy":
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
   },
@@ -24,7 +26,7 @@ const candidateData = [
     "tax-policy":
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
   },
-    {
+  {
     "name": "Donald Trump Sr.",
     "age": 75,
     "party": "Republican",
