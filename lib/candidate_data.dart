c917f2d41c050b23813915e51dd3afaa1adf49c8
ferdinand <ferdinandmwal@gmail.com>
2021-03-06 16:25:10 +0300

    migration & social

--- lib/candidate_data.dart
+++ lib/candidate_data.dart
@@ -31,9 +31,17 @@ const candidateData = [
     "age": 21,
     "party": "Democrat",
     "face": "clinton.jpg",
+    "migration-policy":
+        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+    "social-policy":
+        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
+        "slogan":"Make America Great Again",
     "tax-policy":
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
   },
+
+
+  //!!???? 
   {
     "name": "Donald Trump Sr.",
     "age": 75,
